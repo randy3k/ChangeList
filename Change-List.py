d7d655d8919c585fb908afa03849c5b746c176ac
Randy Lai <shingx1@gmail.com>
2012-11-18 19:31:07 -0800

    add multi cursor support

--- Change-List.py
+++ Change-List.py
@@ -1,26 +1,25 @@
 import sublime, sublime_plugin
 
 # to store edited locations
-if not 'EDTLOC' in globals(): EDTLOC = {}
+if not 'EPOS' in globals(): EPOS = {}
 # to store num of lines
 if not 'FILENOL' in globals(): FILENOL = {}
-# to store current location index
+# to store current index
 if not 'CURINX' in globals(): CURINX = {}
-# to store current location index
-if not 'SLTDLINE' in globals(): SLTDLINE = {}
+if not 'SELROW' in globals(): SELROW = {}
 
 class JumpToChangeCommand(sublime_plugin.TextCommand):
 	def run(self, _, move):
 		view = self.view
 		vid = view.id()
 		curr_row = view.rowcol(view.sel()[0].end())[0]
-		if (not EDTLOC.has_key(vid)) | (not EDTLOC[vid]): return
-		RECINX = len(EDTLOC[vid])-1
+		if (not EPOS.has_key(vid)) | (not EPOS[vid]): return
+		RECINX = len(EPOS[vid])-1
 		# if the cursor has moved away from the recent edited location, set move = 0
-		if (CURINX[vid]==RECINX) & (move==-1) & (curr_row != EDTLOC[vid][RECINX][0]): move = 0
+		if (CURINX[vid]==RECINX) & (move==-1) & (curr_row != EPOS[vid][RECINX][0]): move = 0
 		NEWINX = CURINX[vid]+move
 		if (NEWINX<0)| (NEWINX>RECINX): return
-		pos = EDTLOC[vid][NEWINX]
+		pos = EPOS[vid][NEWINX]
 		region = view.text_point(pos[0],pos[1])
 		view.sel().clear()
 		view.show(region)
@@ -34,73 +33,74 @@ class ChangeList(sublime_plugin.EventListener):
 		vid = view.id()
 		vname = view.file_name()
 		# current num of lines
-		filenol = view.rowcol(view.size())[0]		
+		file_nol = view.rowcol(view.size())[0]		
 		try:		
 			settings = sublime.load_settings('%s.sublime-settings' % __name__)
-			EDTLOC[vid] = [map(int, item.split(",")) for item in settings.get(vname).split("|")]
-			CURINX[vid] = len(EDTLOC[vid])-1
+			EPOS[vid] = [map(int, item.split(",")) for item in settings.get(vname).split("|")]
+			CURINX[vid] = len(EPOS[vid])-1
 		except:
-			EDTLOC[vid] = []
+			EPOS[vid] = []
 			CURINX[vid] = 0
-		FILENOL[vid] = filenol	
+		FILENOL[vid] = file_nol	
 
 	# required for detecting deleted selections
 	def on_selection_modified(self, view):
 		vid = view.id()
 		# get the current multi cursor locations
-		SLTDLINE[vid] = map(lambda s: view.rowcol(s.end())[0] ,view.sel())
+		SELROW[vid] = map(lambda s: int(view.rowcol(s.end())[0]) ,view.sel())
 
 	def on_modified(self, view):
+		if view.is_scratch() or view.settings().get('is_widget'): return
 		vid = view.id()
-		curr_row, curr_col = view.rowcol(view.sel()[0].end())
+		curr_pos = map(lambda s: map(int, view.rowcol(s.end())) ,view.sel())
 		# current num of lines
-		filenol = view.rowcol(view.size())[0]
-		sltdline = map(lambda s: view.rowcol(s.end())[0] ,view.sel())
-		if not EDTLOC.has_key(vid): EDTLOC[vid] = []
-		if not FILENOL.has_key(vid): FILENOL[vid] = filenol
+		file_nol = view.rowcol(view.size())[0]
 
-		if EDTLOC[vid]:
+		if EPOS[vid]:
 			# if num of lines changes
-			if FILENOL[vid] != filenol:
-				deltas = map(lambda x,y: x-y, sltdline, SLTDLINE[vid])
-				deltas = [deltas[0]]+[x - deltas[i - 1] for i, x in enumerate(deltas) if i>0]
+			if FILENOL[vid] != file_nol:
+				deltas = map(lambda x,y: x[0]-y, curr_pos, SELROW[vid])
+				deltas = [int(x - deltas[i-1]) for i,x in enumerate(deltas) if i>0]
+				deltas = [int(file_nol-FILENOL[vid]-sum(deltas))] + deltas
 				print(deltas)
 				for i, delta in enumerate(deltas):
 					# drop pos
-					if delta<0:
-						EDTLOC[vid] = \
-							filter(lambda pos: (pos[0]<=sltdline[i]) | (pos[0]>SLTDLINE[vid][i]), EDTLOC[vid])
+					if (delta<0) & (i==0):
+							EPOS[vid] = \
+								filter(lambda pos: (pos[0]<curr_pos[i][0]) | (pos[0]>SELROW[vid][i]), EPOS[vid])
 					# update pos that is after the current line
-					EDTLOC[vid] = \
-						map(lambda pos: [pos[0]+delta*(pos[0] >= max(SLTDLINE[vid][i],sltdline[i])), pos[1]], EDTLOC[vid])
+					EPOS[vid] = \
+						map(lambda pos: [pos[0]+delta*(pos[0] >= max(SELROW[vid][i],curr_pos[i][0])), pos[1]], EPOS[vid])
 						
 				# drop position if position is invalid
-				EDTLOC[vid] = filter(lambda pos: (pos[0]>=0) & (pos[0]<=filenol), EDTLOC[vid])
+				EPOS[vid] = filter(lambda pos: (pos[0]>=0) & (pos[0]<=file_nol), EPOS[vid])
 				# update num of lines
-				FILENOL[vid] = filenol		
-			
-			RECINX = len(EDTLOC[vid])-1
-			if abs(EDTLOC[vid][RECINX][0] - curr_row)>1:
-				EDTLOC[vid].append([int(curr_row), int(curr_col)])
-			else:
-				EDTLOC[vid][RECINX] = [int(curr_row), int(curr_col)]	
-		else:
-			EDTLOC[vid] = [[int(curr_row), int(curr_col)]]
+				FILENOL[vid] = file_nol		
 
-		if len(EDTLOC[vid])>50:
-			EDTLOC[vid].pop(0)
-		CURINX[vid] = len(EDTLOC[vid])-1			
-		print(EDTLOC[vid])
+		if EPOS[vid]:
+			RECINX = len(EPOS[vid])-1
+			if abs(EPOS[vid][RECINX][0] - curr_pos[0][0])>1:
+				EPOS[vid].append(curr_pos[0])
+			else:
+				EPOS[vid][RECINX] = curr_pos[0]	
+
+			if len(EPOS[vid])>50: EPOS[vid].pop(0)
+		else:
+			EPOS[vid] = [curr_pos[0]]
+
+		CURINX[vid] = len(EPOS[vid])-1
+		print(EPOS[vid])
+
 
 	def on_post_save(self, view):
 		vid = view.id()
 		vname = view.file_name()		
 		settings = sublime.load_settings('%s.sublime-settings' % __name__)
-		settings.set(vname, "|".join([",".join(map(str, item)) for item in EDTLOC[vid]]))	
+		settings.set(vname, "|".join([",".join(map(str, item)) for item in EPOS[vid]]))	
 		sublime.save_settings('%s.sublime-settings' % __name__)
 
 	def on_close(self, view):
 		vid = view.id()	
-		EDTLOC.pop(vid)
+		EPOS.pop(vid)
 		FILENOL.pop(vid)
 		CURINX.pop(vid)
