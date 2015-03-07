7933e28ec2cffa53d8ccd12dd510e31e1738676f
Randy Lai <randy.cs.lai@gmail.com>
2015-03-06 22:03:27 -0800

    fix pep8

--- changelist.py
+++ changelist.py
@@ -1,8 +1,9 @@
-import sublime, sublime_plugin
+import sublime
+import sublime_plugin
 import os
-import sys
 import json
 import codecs
+
 
 class Jfile:
     def __init__(self, fpath, encoding="utf-8"):
@@ -35,11 +36,13 @@ class Jfile:
         f.close()
 
     def remove(self):
-        if os.path.exists(self.fpath): os.remove(self.fpath)
+        if os.path.exists(self.fpath):
+            os.remove(self.fpath)
 
 
 st2 = int(sublime.version()) < 3000
 key_prefix = "cl"
+
 
 # Change List object
 class CList():
@@ -52,7 +55,6 @@ class CList():
     @classmethod
     def get_clist(cls, view):
         vid = view.id()
-        vname = view.file_name()
         if vid in cls.dictionary:
             this_clist = cls.dictionary[vid]
         else:
@@ -68,24 +70,26 @@ class CList():
     def push_key(self):
         view = self.view
         region_list = list(view.sel())
-        if not region_list: return
+        if not region_list:
+            return
 
         if self.key_list:
             last_sel = view.get_regions(self.key_list[-1])
             # dont push key if no jump
-            if last_sel == region_list: return
+            if last_sel == region_list:
+                return
 
             # delete last key if same line
             if len(last_sel) == len(region_list):
-                if all([view.rowcol(i.begin())[0]==view.rowcol(j.begin())[0] \
-                        for i,j in zip(last_sel, region_list)]):
+                if all([view.rowcol(i.begin())[0] == view.rowcol(j.begin())[0]
+                        for i, j in zip(last_sel, region_list)]):
                     self.key_counter -= 1
                     self.key_list.pop(-1)
 
         self.pointer = -1
         key = self.generate_key()
         view.erase_regions(key)
-        view.add_regions(key, region_list ,"")
+        view.add_regions(key, region_list, "")
         self.key_list.append(key)
 
     def generate_key(self):
@@ -100,13 +104,13 @@ class CList():
 
     def reload_keys(self, sel_list=None):
         view = self.view
-        if not sel_list: sel_list = [self.view.get_regions(key) for key in self.key_list]
-        for i,sel in enumerate(sel_list):
+        if not sel_list:
+            sel_list = [self.view.get_regions(key) for key in self.key_list]
+        for i, sel in enumerate(sel_list):
             view.erase_regions(key_prefix+str(i+1))
             view.add_regions(key_prefix+str(i+1), sel, "")
         self.key_list = [key_prefix+str(i+1) for i in range(len(sel_list))]
         self.key_counter = len(sel_list)
-
 
     def trim_keys(self):
         view = self.view
@@ -128,7 +132,8 @@ class CList():
 
     def goto(self, index, show_at_bottom=False):
         view = self.view
-        if index>=0 or index< -len(self.key_list): return
+        if index >= 0 or index < -len(self.key_list):
+            return
         self.pointer = index
         sel = view.get_regions(self.key_list[index])
         view.sel().clear()
@@ -137,21 +142,22 @@ class CList():
         if show_at_bottom:
             view.set_viewport_position((view.viewport_position()[0], 0))
             row, col = view.rowcol(view.sel()[-1].end())
-            view.show(view.text_point(row+5,col), False)
+            view.show(view.text_point(row+5, col), False)
         else:
             view.show_at_center(view.sel()[0])
         # to reactivate cursor
-        view.run_command("move", {"by": "characters", "forward" : False})
-        view.run_command("move", {"by": "characters", "forward" : True})
+        view.run_command("move", {"by": "characters", "forward": False})
+        view.run_command("move", {"by": "characters", "forward": True})
 
     def save(self):
         view = self.view
         vname = view.file_name()
         jfile = Jfile(self.jpath)
         data = jfile.load(default={})
+
         def f(s):
-            return str(s.begin())+","+str(s.end()) if s.begin()!=s.end() else str(s.begin())
-        data[vname] =  {"history": "|".join(
+            return str(s.begin())+","+str(s.end()) if s.begin() != s.end() else str(s.begin())
+        data[vname] = {"history": "|".join(
                 [":".join([f(s) for s in view.get_regions(key)]) for key in self.key_list]
         )}
         jfile.save(data, indent=0)
@@ -161,18 +167,20 @@ class CList():
         vname = view.file_name()
         jfile = Jfile(self.jpath)
         data = jfile.load(default={})
+
         def f(s):
-            return sublime.Region(int(s[0]),int(s[1])) if len(s)==2 \
-                                        else sublime.Region(int(s[0]),int(s[0]))
+            return sublime.Region(int(s[0]), int(s[1])) if len(s) == 2 \
+                                        else sublime.Region(int(s[0]), int(s[0]))
         if vname in data:
-            sel_list = [[f(s.split(",")) for s in sel.split(":") if len(s)>0]\
-                                    for sel in data[vname]['history'].split("|")]
+            sel_list = [[f(s.split(",")) for s in sel.split(":") if len(s) > 0]
+                        for sel in data[vname]['history'].split("|")]
             self.reload_keys(sel_list)
 
 
 class CListener(sublime_plugin.EventListener):
     def on_modified(self, view):
-        if view.is_scratch() or view.settings().get('is_widget'): return
+        if view.is_scratch() or view.settings().get('is_widget'):
+            return
         this_clist = CList.get_clist(view)
         this_clist.remove_empty_keys()
         this_clist.push_key()
@@ -181,19 +189,21 @@ class CListener(sublime_plugin.EventListener):
     def on_post_save(self, view):
         CList.get_clist(view).save()
 
-
     def on_close(self, view):
         vid = view.id()
-        if vid in CList.dictionary: CList.dictionary.pop(vid)
+        if vid in CList.dictionary:
+            CList.dictionary.pop(vid)
 
 
 class JumpToChange(sublime_plugin.TextCommand):
     def run(self, _, **kwargs):
         view = self.view
 
-        if view.is_scratch() or view.settings().get('is_widget'): return
+        if view.is_scratch() or view.settings().get('is_widget'):
+            return
         this_clist = CList.get_clist(view)
-        if not this_clist.key_list: return
+        if not this_clist.key_list:
+            return
         if 'move' in kwargs:
             move = kwargs['move']
             if move == -1 and this_clist.pointer == -1 and \
@@ -204,36 +214,43 @@ class JumpToChange(sublime_plugin.TextCommand):
             index = kwargs['index']
         else:
             return
-        if index>=0 or index< -len(this_clist.key_list): return
+        if index >= 0 or index < -len(this_clist.key_list):
+            return
         show_at_bottom = kwargs['show_at_bottom'] if 'show_at_bottom' in kwargs else False
         this_clist.goto(index, show_at_bottom)
         sublime.status_message("@[%s]" % str(-index-1))
 
+
 class ShowChangeList(sublime_plugin.WindowCommand):
     def run(self):
         view = self.window.active_view()
-        if view.is_scratch() or view.settings().get('is_widget'): return
+        if view.is_scratch() or view.settings().get('is_widget'):
+            return
         this_clist = CList.get_clist(view)
-        if not this_clist.key_list: return
-        def f(i,key):
+        if not this_clist.key_list:
+            return
+
+        def f(i, key):
             begin = view.get_regions(key)[0].begin()
             return "%3d: %s" % (view.rowcol(begin)[0]+1, view.substr(view.line(begin)).strip())
-        display_list = [ f(i,key) for i,key in enumerate(reversed(this_clist.key_list))]
+        display_list = [f(i, key) for i, key in enumerate(reversed(this_clist.key_list))]
         self.savept = [s for s in view.sel()]
         if st2:
-            self.window.show_quick_panel(display_list,
-                    self.on_done, sublime.MONOSPACE_FONT)
+            self.window.show_quick_panel(display_list, self.on_done, sublime.MONOSPACE_FONT)
         else:
-            self.window.show_quick_panel(display_list,
-                    self.on_done, sublime.MONOSPACE_FONT, on_highlight=self.on_done)
+            self.window.show_quick_panel(
+                display_list,
+                self.on_done, sublime.MONOSPACE_FONT, on_highlight=self.on_done
+            )
 
     def on_done(self, action):
         view = self.window.active_view()
-        if action==-1:
+        if action == -1:
             view.sel().clear()
-            for s in self.savept: view.sel().add(s)
+            for s in self.savept:
+                view.sel().add(s)
             return
-        view.run_command("jump_to_change", {"index" : -action-1, 'show_at_bottom': True})
+        view.run_command("jump_to_change", {"index": -action-1, 'show_at_bottom': True})
 
 
 class CleanChangeList(sublime_plugin.WindowCommand):
@@ -244,44 +261,45 @@ class CleanChangeList(sublime_plugin.WindowCommand):
     def run(self):
         self.jpath = os.path.join(sublime.packages_path(), 'User', 'ChangeList.json')
         view = self.window.active_view()
-        if view.is_scratch() or view.settings().get('is_widget'): return
-        try:
-            fname = os.path.basename(view.file_name())
-        except:
-            fname = "untitled"
-        self.show_quick_panel(["Rebuild Change List", "Clean up this file",
-                "Clean up all files"], self.confirm)
+        if view.is_scratch() or view.settings().get('is_widget'):
+            return
+
+        self.show_quick_panel(
+            ["Rebuild Change List", "Clean up this file", "Clean up all files"],
+            self.confirm)
 
     def confirm(self, action):
-        if action<0: return
+        if action < 0:
+            return
         self.action = action
         self.show_quick_panel(["Cancel", "Apply"], self.on_done)
 
     def on_done(self, confirm):
         view = self.window.active_view()
-        if confirm<=0:
+        if confirm <= 0:
             self.run()
             return
 
         action = self.action
         jfile = Jfile(self.jpath)
-        if action==0:
+        if action == 0:
             data = jfile.load(default={})
             for item in [item for item in data if not os.path.exists(item)]:
                 data.pop(item)
             jfile.save(data, indent=0)
             sublime.status_message("Change List is rebuilt.")
-        elif action==1:
+        elif action == 1:
             vid = view.id()
             vname = view.file_name()
-            if vid in CList.dictionary: CList.dictionary.pop(vid)
+            if vid in CList.dictionary:
+                CList.dictionary.pop(vid)
             if vname:
                 data = jfile.load(default={})
                 if vname in data:
                     data.pop(vname)
                     jfile.save(data, indent=0)
             sublime.status_message("Change List for this file is emptied.")
-        elif action==2:
+        elif action == 2:
             CList.dictionary = {}
             jfile.remove()
             sublime.status_message("Change List is emptied.")
